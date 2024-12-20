683db07c1320ab1d094f70db9ebdbcb8721e0675
Seunghyun Lee <148722997+seunghyun-stats@users.noreply.github.com>
2024-12-19 19:38:57 -0500

    Delete Poisson SAEM directory

--- Poisson SAEM/nchoosek_prac.m
+++
@@ -1,9 +0,0 @@
-function [y] = nchoosek_prac(n, k)
-
-
-
-y = exp(sum(log((n-k+1):n)) - sum(log(1:k)));
-
-
-
-end
--- Poisson SAEM/thres.m
+++
@@ -1,5 +0,0 @@
-function X_th = thres(X, tau)
-    index = abs(X) < tau;
-    X_th = X;
-    X_th(index) = 0;
-end
