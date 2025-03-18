508f349b25bf71ee0f6fee09c05f39ba4c81f449
Seunghyun Lee <148722997+seunghyun-stats@users.noreply.github.com>
2025-03-18 16:38:52 -0400

    Delete real data/TIMSS/TIMSS directory

--- real data/TIMSS/TIMSS/supplementary functions/Fun_1_confirm.m
+++
@@ -1,8 +0,0 @@
-function [f] = Fun_1_confirm(X_j, A1_j, psi)  
-
-f = @loglike; % NEW
-    function [obj] = loglike(x) 
-        eta_j = x * A1_j';
-        obj = sum(psi .* (X_j- eta_j).^2, 'all');
-    end
-end
