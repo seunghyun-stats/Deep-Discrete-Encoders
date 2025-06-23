9801c15d9887fa0e17bab948be70dd6e92ef8f0f
Seunghyun Lee <148722997+seunghyun-stats@users.noreply.github.com>
2025-06-23 11:53:29 -0400

    Update TIMSS analysis.m

--- real data/TIMSS/TIMSS analysis.m
+++ real data/TIMSS/TIMSS analysis.m
@@ -1,24 +1,21 @@
-%% data preprocessing
-K2 = 1; % K2 = 2;
+%% import data
+addpath('C:\Users\leesa\Documents\MATLAB\Deep parametric model\GitHub codes\Utilities')
 K1 = 7;
-J = 29;
 
 G1 = importdata("Q.csv").data;
-Y = importdata("time.csv");
+X = importdata("TIMSS.csv");
 
-N = size(Y,1);
+R = X(:, 1:29);  % response accuracy
+X = X(:, 30:58); % response time
 
-X = log(Y);
-row_nonmissing = sum(isnan(X), 2) == 0;
-X_adj = X(row_nonmissing, :);
-X = X_adj;
+N = size(X,1);
+J = size(X,2);
 
 
 %% main estimation
-C = 30;
-B10 = zeros(J, K1+1, C);
-B20 = zeros(K1, K2+1, C); 
-gamma_0 = zeros(J, C); prop_0 = zeros(K2,C);
+C = 40;
+B10_time = zeros(J, K1+1, C); B10_resp = zeros(J, K1+1, C);
+gamma_0 = zeros(J, C);
 loglik_vec = zeros(C,1);
 tol = 1;
 
@@ -26,43 +23,62 @@ K2 = 1; G2 = ones(K1,1);
 B20 = zeros(K1, K2+1, C); prop_0 = zeros(K2,C);
 parfor(c = 1:C, 4)
     rng(c);
-    prop_in = 0.2 + 0.4*rand(1,K2);
+    prop_in = 0.4 + 0.4*rand(1,K2);
     gamma_in = 0.4*ones(J,1);
-    B1_in = [1.5*ones(J,1)+rand(J,1), G1.*(rand(J, K1)+0.5)];
-    B2_in =[-2*ones(K1,1)+rand(K1,1), G2.*(2*rand(K1, K2)+0.5)];
-    
-    % [prop_in, B1_in, B2_in, gamma_in, A1_in, A2_in, ...
-    %    ~, ~] = Normal_init_data(X_adj, K1, K1, K2, K2, G1, G2, 0.01);
+    B1_time_in = [2*ones(J,1)+rand(J,1), G1.*(rand(J, K1)+0.5)];
+    B1_resp_in = [-1*ones(J,1)+rand(J,1), G1.*(rand(J, K1))];
+    B2_in =[-1.5*ones(K1,1)+rand(K1,1), G2.*(2*rand(K1, K2)+0.5)];
 
-    [prop, B1, B2, gamma, loglik, itera] = get_EM_normal_confirm(X, prop_in, B1_in, B2_in, gamma_in, G1, G2);
+    [prop, B1_time, B1_resp, B2, gamma, loglik, itera] = get_EM_multimodal_confirm(X, R, prop_in, B1_time_in, B1_resp_in, B2_in, gamma_in, G1, G2);
 
-    prop_0(:,c) = prop; B10(:,:,c) = B1; B20(:,:,c) = B2; gamma_0(:,c) = gamma;
+    prop_0(:,c) = prop; B10_time(:,:,c) = B1_time; B10_resp(:,:,c) = B1_resp; B20(:,:,c) = B2; gamma_0(:,c) = gamma;
     loglik_vec(c) = loglik;
 end
 
 [~,c] = max(loglik_vec);
-prop = prop_0(:,c) 
-B1 = B10(:,:,c)
-B2 = B20(:,:,c) 
-gamma = gamma_0(:,c)
-
-df = K2 + J + sum(G1, 'all') + sum(G2, 'all');
-BIC = -2*max(loglik_vec) + 2*df*log(N);
+prop = prop_0(:,c);
+B1_time = B10_time(:,:,c);
+B1_resp = B10_resp(:,:,c);
+B2 = B20(:,:,c);
+gamma = gamma_0(:,c);
 
 
-%% compute phi and estimate the latent variables
+
+%% visualize estimated coefficients
+figure;
+h = heatmap(0:4, 1:J, round(B1_time(1:J,1:5),2));
+h.Title = 'Response time';
+h.XLabel = 'attribue indices';
+h.XData = ["intercept","Number","Algebra", 'Geometry', "Data&Prob"];
+s = struct(h);
+s.XAxis.TickLabelRotation = 30;
+print('-r300', 'RT', '-dpng')
+
+figure;
+h = heatmap(0:4, 1:J, round(B1_resp(1:J,1:5),2));
+h.Title = 'Response accuracy';
+h.XLabel = 'attribue indices';
+h.XData = ["intercept","Number","Algebra", 'Geometry', "Data&Prob"];
+s = struct(h);
+s.XAxis.TickLabelRotation = 30;
+print('-r300', 'RA', '-dpng')
+
+
+
+%% estimate latent configurations
 phi = zeros(N, 2^K1, 2^K2);
-
+A1 = binary(0:(2^K1-1), K1); A1_long = [ones(2^K1,1), A1];
 A2 = binary(0:(2^K2-1), K2); A2_long = [ones(2^K2,1), A2];
-eta_1 = B1*A1_long';
+eta_time_1 = B1_time*A1_long'; eta_resp_1 = B1_resp*A1_long';
 eta_2 = B2*A2_long';
 
 for i = 1:N
     exponent = zeros(2^K1, 2^K2);
     for a = 1:2^K1
         for b = 1:2^K2
-            exponent(a,b) = -sum((X(i,:)'- eta_1(:,a)).^2./gamma)/2 + A1(a,:) * eta_2(:,b) - sum(log(1+exp(eta_2(:,b)))) ...
-                            + log(prop ./ (1-prop)) * A2(b,:)';
+            exponent(a,b) = R(i,:)*eta_resp_1(:,a) - sum(log(1+exp(eta_resp_1(:,a)))) ...
+                    -sum((X(i,:)'- eta_time_1(:,a)).^2./gamma)/2 + A1(a,:) * eta_2(:,b) - sum(log(1+exp(eta_2(:,b)))) ...
+                    + log(prop ./ (1-prop))' * A2(b,:)';
         end
     end
     phi(i, :, :) = exp(exponent - max(max(exponent)));
@@ -77,14 +93,10 @@ for i = 1:N
         psi(i, b) = sum(phi(i,:,b), "all");
     end
 end
-
 for i = 1:N
     [~, b] = max(psi(i,:));
     A2_est(i,:) = binary(b-1, K2);
 end
-mean(A2_est, 1);
-save("A2_est.csv", A2_est)
-
 
 % A1
 psi_a = zeros(N, 2^K1); A1_est = zeros(N, K1);
@@ -93,10 +105,21 @@ for i = 1:N
         psi_a(i, a) = sum(phi(i,a,:), "all");
     end
 end
-
 for i = 1:N
     [~, b] = max(psi_a(i,:));
     A1_est(i,:) = binary(b-1, K1);
 end
-mean(A1_est, 1);
 
+%% comparison with held-out survey response
+% value: 1 [Agree a lot], 2 [Agree a little], 3 [Disagree a little], 4 [Disagree a lot]
+
+like_math = readmatrix("survey_response.csv");
+
+mean_vec_2 = zeros(4, K2);
+mean_vec_1 = zeros(4, K1);
+
+for a = 1:4
+    ind_a = find(like_math(:,c) == a);
+    mean_vec_2(a,:) = mean(A2_est(ind_a, :));
+    mean_vec_1(a,:) = mean(A1_est(ind_a, :));
+end
